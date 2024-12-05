feb94bdc1787ca7bd466173d8699334961b95a9a
Dashurja <daghuria@gmail.com>
2024-12-05 20:49:53 +0300

    index.php

--- index.php
+++ index.php
@@ -3,16 +3,16 @@
 include_once 'db.class.php';
 
 // хост БД
-define('db_host','localhost:3306');
+define('db_host','localhost');
 
 // Имя БД
 define('db_name','dashurja');
 
 // Пользователь БД
-define('db_user','admin');
+define('db_user','root');
 
 // Пароль БД
 define('db_pass','admin_password');
 
 // Обявление класса для подключения к бд
-$db = new DB_class('localhost','dashurja','admin','admin_password');
+$db = new DB_class(db_host,db_name,db_user,db_pass);
